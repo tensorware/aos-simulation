db9570eb97947cd6a791f0b6f85f99d4a7bdadb1
Francis Seits <francis.seits@tensorware.net>
2021-03-08 23:50:03 +0100

    remove positions

--- js/objects/person.js
+++ js/objects/person.js
@@ -2,26 +2,20 @@ class Person {
     constructor(config) {
         this.config = config;
 
-        const margin = 1;
-        this.position = {
-            x: randomInt(-this.config.size / 2 + margin, this.config.size / 2 - margin),
-            z: randomInt(-this.config.size / 2 + margin, this.config.size / 2 - margin)
-        };
-
         const width = 1;
         const height = 2;
-        const widthSegments = width * 5;
-        const heightSegments = height * 5;
+        const widthSegments = width * 10;
+        const heightSegments = height * 10;
 
         const planeGeometry = new THREE.PlaneGeometry(width, height, widthSegments, heightSegments);
-        planeGeometry.rotateX(-Math.PI / 2).translate(this.position.x, 0.10, this.position.z);
+        planeGeometry.rotateX(-Math.PI / 2).translate(0, 0.10, 0);
         const planeMaterial = new THREE.MeshStandardMaterial({ color: 0xd05bf5 });
 
         const wireGeometry = new THREE.WireframeGeometry(planeGeometry);
-        const wireMaterial = new THREE.LineBasicMaterial({ color: 0x885bf5 });
-        const wireFrame = new THREE.LineSegments(wireGeometry, wireMaterial);
+        const wireMaterial = new THREE.LineBasicMaterial({ color: 0x885b95 });
 
+        this.wire = new THREE.LineSegments(wireGeometry, wireMaterial);
         this.mesh = new THREE.Mesh(planeGeometry, planeMaterial);
-        this.mesh.add(wireFrame);
+        this.mesh.add(this.wire);
     }
 }
