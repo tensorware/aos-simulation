951a8a950b4a951df5dcc5a96c969965c4f7f7f8
Francis Seits <francis.seits@tensorware.net>
2021-04-05 23:51:44 +0200

    viewport

--- js/objects/stage.js
+++ js/objects/stage.js
@@ -7,26 +7,29 @@ class Stage {
             this.font = font;
             this.scene = new THREE.Scene();
 
-            this.camera = new THREE.PerspectiveCamera(100, this.root.clientWidth / this.root.clientHeight, 1, 1000);
-            this.camera.position.set(0, 30, 40);
+            this.directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+            this.directionalLight.position.set(100, 100, 100);
+            this.ambientLight = new THREE.AmbientLight(0x444444, 2);
+
+            this.camera = new THREE.PerspectiveCamera(90, this.root.clientWidth / this.root.clientHeight, 0.1, 1000);
+            this.camera.layers.enable(0);
+            this.camera.layers.enable(1);
+            this.camera.position.set(0, 45, 65);
+            this.camera.add(this.directionalLight);
+            this.camera.add(this.ambientLight);
             this.scene.add(this.camera);
 
             this.renderer = new THREE.WebGLRenderer({ antialias: true });
             this.renderer.setSize(this.root.clientWidth, this.root.clientHeight);
             this.renderer.setPixelRatio(window.devicePixelRatio);
+            this.renderer.autoClear = false;
 
             this.controls = new THREE.MapControls(this.camera, this.renderer.domElement);
-            this.controls.minDistance = 1;
-            this.controls.maxDistance = 300;
+            this.controls.minDistance = 0.1;
+            this.controls.maxDistance = 500;
             this.controls.autoRotateSpeed = 1;
             this.controls.autoRotate = false;
             this.controls.enablePan = true;
-
-            const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-            directionalLight.position.set(100, 100, 50);
-            const ambientLight = new THREE.AmbientLight(0x444444, 2);
-            this.camera.add(directionalLight);
-            this.camera.add(ambientLight);
 
             this.stats = new Stats();
             this.root.querySelector('#info').appendChild(this.stats.dom);
@@ -43,21 +46,25 @@ class Stage {
         }).bind(this));
     }
 
-    update() {
-        this.camera.aspect = this.root.clientWidth / this.root.clientHeight;
-        this.camera.updateProjectionMatrix();
-        this.renderer.setSize(this.root.clientWidth, this.root.clientHeight);
-    }
-
     animate() {
         requestAnimationFrame(this.animate);
         this.controls.update();
         this.render();
     }
 
+    update() {
+        this.renderer.setSize(this.root.clientWidth, this.root.clientHeight);
+        this.camera.aspect = this.root.clientWidth / this.root.clientHeight;
+        this.camera.updateProjectionMatrix();
+    }
+
     render() {
         this.stats.begin();
+
+        this.renderer.clear();
+        this.renderer.setViewport(0, 0, this.root.clientWidth, this.root.clientHeight);
         this.renderer.render(this.scene, this.camera);
+
         this.stats.end();
     }
 }
