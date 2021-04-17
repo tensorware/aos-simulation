234dd1d0a680d30514b51865c3af58aef4a9f717
Francis Seits <francis.seits@tensorware.net>
2021-04-17 22:02:18 +0200

    update worker

--- js/utils/worker.js
+++ js/utils/worker.js
@@ -1,23 +1,27 @@
-importScripts('../objects/tree.js');
+importScripts('../utils/helper.js', '../objects/tree.js');
 
 const getTrees = (configs, chunks) => {
     const trees = [];
+
     configs.forEach((config, i) => {
         trees.push(new Tree(config));
+
+        // chunk results for continuity
         if (!((i + 1) % chunks)) {
             self.postMessage(trees);
             trees.splice(0, trees.length);
         }
     });
+
     self.postMessage(trees);
 }
 
 self.onmessage = (e) => {
-    if (e.data) {
-        const method = e.data.method;
-        const params = e.data.params;
-        if (method === 'getTrees') {
-            getTrees(params.configs || [], params.chunks);
-        }
+    const method = e.data.method;
+    const params = e.data.params;
+
+    // call methods
+    if (method === 'getTrees') {
+        getTrees(params.configs || [], params.chunks);
     }
 }
